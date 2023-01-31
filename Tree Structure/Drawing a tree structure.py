06fab742d07cd76a84b92316b7296dae6037a5c1
Pureddy Lavanya <97614700+PureddyLavanya@users.noreply.github.com>
2023-01-31 22:51:47 +0530

    Add files via upload

---
+++ Tree Structure/Drawing a tree structure.py
@@ -0,0 +1,26 @@
+import turtle
+t=turtle.Turtle()
+t.screen.bgcolor('black')
+t.pensize(3)
+t.color('red')
+t.left(90)
+t.backward(100)
+t.speed(700)
+t.shape('turtle')
+
+def tree(i):
+    if i<10:
+        return
+    else:
+        t.forward(i)
+        t.color('orange')
+        t.circle(2)
+        t.color('brown')
+        t.left(30)
+        tree(3*i/4)
+        t.right(60)
+        tree(3*i/4)
+        t.left(30)
+        t.backward(i)
+tree(100)
+turtle.done()
